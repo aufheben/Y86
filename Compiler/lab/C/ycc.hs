23252795d07714d763d3122c4114477e5c5fd527
Yifan Yu <yvifan@gmail.com>
2018-01-18 00:13:51 +0200

    Build function table and find "main" function

--- Compiler/lab/C/ycc.hs
+++ Compiler/lab/C/ycc.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Data.Monoid ((<>))
 import System.IO ( stdin, hGetContents )
 import System.Environment ( getArgs, getProgName )
 import System.Exit ( exitFailure, exitSuccess )
@@ -12,8 +13,11 @@ import AbsC
 
 import ErrM
 
+import qualified Data.Map as M
+
 type ParseFun = [Token] -> Err Program
 
+do_compile :: [External_declaration] -> IO ()
 do_compile [] = return ()
 do_compile (decl:ext_decls) = do
   case decl of
@@ -31,8 +35,28 @@ do_compile (decl:ext_decls) = do
       putStrLn "Global Declarators"
   do_compile ext_decls
 
+buildFuncTable :: [External_declaration] -> M.Map String External_declaration
+buildFuncTable xs = build_func_table xs M.empty
+  where
+  build_func_table [] m = m
+  build_func_table (decl:ext_decls) m =
+    case decl of
+      Afunc (NewFunc _ (NoPointer (NewFuncDec (Name (Ident name)) _)) _) ->
+        let m' = M.insert name decl m
+        in build_func_table ext_decls m'
+      _ -> build_func_table ext_decls m
+
 compile :: Program -> IO ()
-compile (Progr ext_decls) = do_compile ext_decls
+compile (Progr ext_decls) = do
+  -- build function table
+  let func_table = buildFuncTable ext_decls
+  putStrLn $ "# " <> show (M.size func_table) <> " functions defined"
+  -- look for "main"
+  case M.lookup "main" func_table of
+    Just main_decl -> do
+      putStrLn "main function found"
+      do_compile ext_decls
+    _ -> putStrLn "main function not defined"
 
 runFile :: ParseFun -> FilePath -> IO ()
 runFile p f = putStrLn f >> readFile f >>= run p
